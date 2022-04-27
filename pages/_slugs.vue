5b381cc344acc523798b1660216678eb8ae0ed66
VEXORteCHc <kacperadam10@gmail.com>
2022-04-27 20:31:39 +0200

    Initial commit

---
+++ pages/_slugs.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ slugs }}</h1>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const slugs = params.slugs
+    return { slugs }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    {{ About }}
+    <nuxt-link to="/">Home!</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      title: 'About',
+    }
+  },
+}
+</script>
+
+<style>
+</style>
